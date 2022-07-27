e4bd80097b38fdb9f6ed709380c0a088427ca944
LuisRemote <ortizangeles.mx@gmail.com>
2022-07-26 19:25:20 -0500

    oop4 added, it's about subclases

--- oop.py
+++ oop.py
@@ -1,9 +1,10 @@
 # Python Object-Oriented Programming
+import datetime
 
 class Employee:
 
     num_of_emps = 0
-    raise_amount = 1.04
+    raise_amt = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
@@ -19,15 +20,27 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
-print(Employee.num_of_emps)
-
-
-
-
+my_date = datetime.date(2016, 7, 11)
+print(Employee.is_workday(my_date))
 
 
 
